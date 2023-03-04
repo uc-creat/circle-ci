bf1a1b22068a4d4e9341a38858a0b2ac208bd4cd
Utkarsh Chauhan <60755716+uc-creat@users.noreply.github.com>
2023-03-04 11:14:34 +0530

    Update Adding.java

--- src/main/java/Adding.java
+++ src/main/java/Adding.java
@@ -1,5 +1,5 @@
 public class Adding {
-    public int findSum(int numberOne, int numberTwo) {
-        return numberOne+numberTwo;
+    public int findSum(int numberOne, int numberTwoo) {
+        return numberOne+numberTwoo;
     }
 }
